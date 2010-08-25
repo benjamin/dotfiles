c9a21ac80922f2cfcd619b777e0f53338346ab40
Benjamin Birnbaum <benjamin@wobolando.com>
2010-08-25 10:33:48 +1000

    Added a git basher to allow me to ignore/uningore and list ignored files

--- support/bashbot/bashers/git.rb
+++ support/bashbot/bashers/git.rb
@@ -6,4 +6,16 @@ Bashers.define "Git" do
     clone_location = clone_location.empty? ? nil : clone_location.strip
     bash.run("git", "clone", git_url, clone_location)
   end
+
+  handler /^git-ignore (.*)$/ do |file|
+    bash.run("git", "update-index", "--assume-unchanged", file)
+  end
+
+  handler /^git-unignore (.*)$/ do |file|
+    bash.run("git", "update-index", "--no-assume-unchanged", file)
+  end
+
+  handler /^git-list-ignored$/ do
+    bash.run("git ls-files -v | grep ^[a-z]")
+  end
 end
